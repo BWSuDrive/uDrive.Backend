014b8673f30a31b2cfc40810a094fa176163507d
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-20 10:42:19 +0200

    chore: generalize controllers

---
+++ src/uDrive.Backend.Api/Controllers/Abstract/AnonymousController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uDrive.Backend.Model;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace uDrive.Backend.Api.Controllers.Abstract;
+
+[Produces(Application.Json)]
+[Consumes(Application.Json)]
+[Route("api/[controller]")]
+[ApiController]
+public class AnonymousController<TEntity> : ControllerBase where TEntity : class, IEntity
+{
+
+   protected ILogger _logger;
+
+    private static ApplicationDbContext _context;
+    public IQueryable<TEntity> Entities => _context.Set<TEntity>().AsQueryable();
+
+    public AnonymousController(ILogger<AnonymousController<TEntity>> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async ValueTask<IQueryable<TEntity>> Get()
+    {
+        return Entities;
+    }
+}
