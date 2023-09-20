483f227c61b2f432c245ef44cc1bf263931c623b
Ben Bigalke <bbigalkecceh@gmail.com>
2023-09-20 14:56:16 +0200

    Deployment/api (#13)
    * chore: update pages
    
    * chore: update readme
    
    * chore: generalize controllers

--- src/uDrive.Backend.Api/Controllers/Weekdays.cs
+++ src/uDrive.Backend.Api/Controllers/Weekdays.cs
@@ -1,37 +1,19 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
+using uDrive.Backend.Api.Controllers.Abstract;
 using uDrive.Backend.Model;
 using uDrive.Backend.Model.Entities;
 
 namespace uDrive.Backend.Api.Controllers;
 
-[ApiController]
-[Route("[controller]")]
 [Authorize(Roles = "Secretary")]
-
-public class Weekdays : ControllerBase
+public class WeekdaysController : SecretaryController<Weekday>
 {
-    private readonly SignInManager<Person> _signInManager;
-    private readonly ILogger<Login> _logger;
-    private readonly UserManager<Person> _userManager;
-    private readonly IConfiguration _configuration;
-    private static ApplicationDbContext _context;
-
-    public Weekdays(SignInManager<Person> signInManager, UserManager<Person> userManager, ILogger<Login> logger, IConfiguration configuration, ApplicationDbContext context)
-    {
-        _signInManager = signInManager;
-        _logger = logger;
-        _userManager = userManager;
-        _configuration = configuration;
-        _context = context;
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> GetAsync()
-    {
-        var res = _context.Weekdays;
-        return Ok(res);
-    }
-
+    public WeekdaysController(
+    ILogger<WeekdaysController> logger,
+    ApplicationDbContext context
+)
+    : base(logger, context) { }
 }
