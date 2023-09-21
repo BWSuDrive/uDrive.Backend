80453f5a63c1d7213b01dd75986c21bd6ab3c1e2
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-21 14:45:44 +0200

    chore: add distance controller

---
+++ src/uDrive.Backend.Model/DTO/GeocoordinatesDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uDrive.Backend.Model.DTO;
+
+public class GeocoordinatesDTO
+{
+    public string DriverPos { get; set; }
+    public string PersonPos { get; set; }
+}
