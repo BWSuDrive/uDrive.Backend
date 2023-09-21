edbe28a3dab4b6220e5e52688d8769f09afca735
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-21 14:45:44 +0200

    chore: add distance controller

---
+++ src/uDrive.Backend.Api/Controllers/DrivingLicencesController.cs
@@ -0,0 +1,20 @@
+﻿using uDrive.Backend.Api.Controllers.Abstract;
+using uDrive.Backend.Model.Entities;
+using uDrive.Backend.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uDrive.Backend.Api.Controllers
+{
+    public class DrivingLicencesController : SecretaryRoleController<DrivingLicence>
+    {
+        public DrivingLicencesController(
+        ILogger<DrivingLicencesController> logger,
+        ApplicationDbContext context
+    )
+        : base(logger, context) { }
+    }
+
+
+    //[HttpPost("/AddDriver/{key}")]
+    //public async Task<IActionResult> PostAddDriver()
+}
