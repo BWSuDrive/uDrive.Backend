5243dc4a86bc318b25d84520f443be46b4ce5be8
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-26 11:01:43 +0200

    chore: add Driver verify

--- src/uDrive.Backend.Pages/Areas/Secretariat/Pages/Drivers/Index.cshtml.cs
+++ src/uDrive.Backend.Pages/Areas/Secretariat/Pages/Drivers/Index.cshtml.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using uDrive.Backend.Model.Entities;
 using uDrive.Backend.Model;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace uDrive.Backend.Pages.Areas.Secretariat.Pages.Drivers
 {
@@ -28,12 +30,57 @@ namespace uDrive.Backend.Pages.Areas.Secretariat.Pages.Drivers
             _context = context;
         }
 
+        [BindProperty]
         public List<Person> Users { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public Driver NewDriver { get; set; }
+
+        [BindProperty]
+        public DrivingLicence Licence { get; set; }
+        public async Task OnGetAsync()
         {
 
-            Users = _context.Persons.ToList();
+
+           await PopulateAsync();
+        }
+
+        public async Task PopulateAsync() {
+            var getUsers = await _context.Persons.AsTracking().Include(drivers => drivers.Drivers).ToListAsync();
+            var p = getUsers.Where(x => !x.Drivers.Any());
+            Users = p.ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+         
+
+            var addLicence = await _context.DrivingLicences.AddAsync(Licence).ConfigureAwait(false);
+            var saveLicence = addLicence.Entity;
+            if (saveLicence is null)
+            {
+                return NotFound();
+            }
+
+            NewDriver.IdDrivinglicense = saveLicence.Id;
+
+            var addDriver = await _context.Drivers.AddAsync(NewDriver).ConfigureAwait(false);
+            var saveDriver = addDriver.Entity;
+            if (saveDriver is null)
+            {
+                return NotFound();
+            }
+            var us = await _userManager.FindByIdAsync(NewDriver.IdPerson).ConfigureAwait(false);
+            //// var us = await _context.Persons. FindAsync(x => x.Id == NewDriver.IdPerson).FirstOrDefaultAsync().ConfigureAwait(false);
+            if (us is null)
+            {
+                return NotFound();
+            }
+
+            await _userManager.AddToRoleAsync(us, UDriveRoles.Driver);
+            await _context.SaveChangesAsync();
+            await PopulateAsync();
+            return Page();
 
         }
     }
