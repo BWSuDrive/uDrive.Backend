1ac1edc5880f43d81791b88ed7cb85e9b8387df4
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-26 13:57:24 +0200

    fix: register

---
+++ tests/uDrive.Backend.Api.Test.Integration/Abstractions/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using NetEvolve.Extensions.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uDrive.Backend.Api.Test.Integration.Abstractions;
+
+[ExcludeFromCodeCoverage]
+[IntegrationTest]
+[TestFixture]
+[Parallelizable]
+public abstract class TestBase : ContinuousTestBase
+{
+    protected TestBaseFactory TestFactory{ get; private set; }
+
+    [OneTimeSetUp]
+    public async Task InitAsync()
+    {
+
+        TestFactory = new TestBaseFactory();
+    }
+}
