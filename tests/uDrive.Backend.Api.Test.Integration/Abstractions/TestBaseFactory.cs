fabd47abbd97025ff3f0046976d3cf0024bafa2e
Benjamin Bigalke <benjamin.bigalke@cgi.com>
2023-09-26 13:57:24 +0200

    fix: register

---
+++ tests/uDrive.Backend.Api.Test.Integration/Abstractions/TestBaseFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NetEvolve.Extensions.NUnit;
+
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace uDrive.Backend.Api.Test.Integration.Abstractions;
+
+public sealed class TestBaseFactory : WebApplicationFactory<Program>
+{
+   // public async Task InitAsync() => await _container.StartAsync().ConfigureAwait(false);
+
+
+}
